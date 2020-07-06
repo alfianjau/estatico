f5c81254b663392163253bfcf886fb4c62423219
alfianjau <alfianjauhari@gmail.com>
2020-07-07 00:34:34 +0700

    component: Agent Page listing

---
+++ src/components/Particle/AgentPage/AgentListingCard.jsx
@@ -0,0 +1,63 @@
+/* eslint-disable no-nested-ternary */
+// Card Element
+
+import React, { useState } from 'react'
+import { Box, Text } from '@chakra-ui/core'
+import DImage from '../../Atom/DImage/DImage'
+import { FiMapPin } from 'react-icons/fi'
+import '../../Atom/CardFlex/CardFlex.css'
+
+function ListingCard(props) {
+  const [hasFloatingBox] = useState(props.hasFloatingBox)
+  const [hasMapPin] = useState(props.hasMapPin)
+
+  return (
+    <Box
+      className="card team-card"
+      overflow="hidden"
+      mx={{ md: '1rem' }}
+      my={6}
+      width={{ sm: '100%', md: 1 / 3 }}
+    >
+      <DImage alt={props.imageAlt} filename={props.imageSrc} />
+      {hasFloatingBox && (
+        <Box
+          className="card__info--floating"
+          mt="-4rem"
+          mx="auto"
+          left="0"
+          right="0"
+          zIndex="docked"
+          p="4rem"
+          width="80%"
+          textAlign="center"
+          fontSize={{ sm: '2xl', md: '4xl' }}
+          fontWeight="bold"
+          lineHeight="tight"
+          background="white"
+          position="relative"
+        >
+          <Text>{props.floatingText}</Text>
+        </Box>
+      )}
+      {hasMapPin && (
+        <Box
+          className="card__map-pin"
+          mt="-5rem"
+          ml="1rem"
+          mr="auto"
+          left="0"
+          textAlign="center"
+          fontSize={{ sm: '2xl', md: '4xl' }}
+          lineHeight="tight"
+          position="absolute"
+        >
+          <FiMapPin color="white" />
+        </Box>
+      )}
+      {props.children}
+    </Box>
+  )
+}
+
+export default ListingCard
