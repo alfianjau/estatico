0b8610e62fac140ec4ee62781b861dd8b8d01667
alfianjau <alfianjauhari@gmail.com>
2020-07-08 19:38:24 +0700

    feature: add filtered function and some data integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,6 +84,31 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-source-apiserver',
+      options: {
+        // Type prefix of entities from server
+        typePrefix: 'bridge__',
+
+        // The url, this should be the endpoint you are attempting to pull data from
+        url: `${process.env.BASE_URL}${process.env.dataset}agents`,
+
+        method: 'get',
+
+        headers: {
+          Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+          // 'Content-Type': 'application/json',
+        },
+
+        // Name of the data to be downloaded.  Will show in graphQL or be saved to a file
+        // using this name. i.e. posts.json
+        name: 'agents',
+
+        // schemaType: postType,
+        params: {},
+        verboseOutput: true,
+      },
+    },
+    {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
