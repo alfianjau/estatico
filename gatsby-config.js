0487303b072b681b4b72a32329e38ff5ff4f3e10
alfianjau <alfianjauhari@gmail.com>
2020-07-24 08:24:25 +0700

    add maskable icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -201,12 +201,12 @@ module.exports = {
         breakpointsMaxImages: 3,
       },
     }, */
-    {
+    /* {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: config.googleAnalyticsID,
       },
-    },
+    }, */
     {
       resolve: 'gatsby-plugin-nprogress',
       options: {
@@ -216,7 +216,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-catch-links',
-    'gatsby-plugin-twitter',
+    // 'gatsby-plugin-twitter',
     'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-manifest',
@@ -240,6 +240,12 @@ module.exports = {
             sizes: '512x512',
             type: 'image/png',
           },
+          {
+            src: 'static/logos/maskable_icon.png',
+            sizes: '196x196',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
         ],
       },
     },
@@ -254,80 +260,7 @@ module.exports = {
         includeRobots: false,
       },
     },
-    {
-      resolve: 'gatsby-plugin-feed',
-      options: {
-        setup(ref) {
-          const ret = ref.query.site.siteMetadata.rssMetadata
-          ret.allMarkdownRemark = ref.query.allMarkdownRemark
-          ret.generator = 'GatsbyJS Advanced Starter'
-          return ret
-        },
-        query: `
-        {
-          site {
-            siteMetadata {
-              rssMetadata {
-                site_url
-                feed_url
-                title
-                description
-                image_url
-                copyright
-              }
-            }
-          }
-        }
-      `,
-        feeds: [
-          {
-            serialize(ctx) {
-              const { rssMetadata } = ctx.query.site.siteMetadata
-              return ctx.query.allMarkdownRemark.edges.map((edge) => ({
-                categories: edge.node.frontmatter.tags,
-                date: edge.node.fields.date,
-                title: edge.node.frontmatter.title,
-                description: edge.node.excerpt,
-                url: rssMetadata.site_url + edge.node.fields.slug,
-                guid: rssMetadata.site_url + edge.node.fields.slug,
-                custom_elements: [
-                  { 'content:encoded': edge.node.html },
-                  { author: config.userEmail },
-                ],
-              }))
-            },
-            query: `
-            {
-              allMarkdownRemark(
-                limit: 1000,
-                sort: { order: DESC, fields: [fields___date] },
-              ) {
-                edges {
-                  node {
-                    excerpt
-                    html
-                    timeToRead
-                    fields {
-                      slug
-                      date
-                    }
-                    frontmatter {
-                      title
-                      cover
-                      date
-                      category
-                      tags
-                    }
-                  }
-                }
-              }
-            }
-          `,
-            output: config.siteRss,
-            title: config.siteRssTitle,
-          },
-        ],
-      },
-    },
   ],
 }
+
+
